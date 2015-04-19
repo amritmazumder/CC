1c4716a51b2535011e98ebebf4f8eef0168c7141
Aashman <agoghari@risd.edu>
2015-04-19 18:24:39 -0400

    force1

--- forcelayout/backup.js
+++ forcelayout/backup.js
@@ -1,7 +1,47 @@
+var graph = {
+  "nodes": [
+    {"x": 469, "y": 410},
+    {"x": 493, "y": 364},
+    {"x": 442, "y": 365},
+    {"x": 467, "y": 314},
+    {"x": 477, "y": 248},
+    {"x": 425, "y": 207},
+    {"x": 402, "y": 155},
+    {"x": 369, "y": 196},
+    {"x": 350, "y": 148},
+    {"x": 539, "y": 222},
+    {"x": 594, "y": 235},
+    {"x": 582, "y": 185},
+    {"x": 633, "y": 200}
+  ],
+  "links": [
+    {"source":  0, "target":  1},
+    {"source":  1, "target":  2},
+    {"source":  2, "target":  0},
+    {"source":  1, "target":  3},
+    {"source":  3, "target":  2},
+    {"source":  3, "target":  4},
+    {"source":  4, "target":  5},
+    {"source":  5, "target":  6},
+    {"source":  5, "target":  7},
+    {"source":  6, "target":  7},
+    {"source":  6, "target":  8},
+    {"source":  7, "target":  8},
+    {"source":  9, "target":  4},
+    {"source":  9, "target": 11},
+    {"source":  9, "target": 10},
+    {"source": 10, "target": 11},
+    {"source": 11, "target": 12},
+    {"source": 12, "target": 10}
+  ]
+}
+
+
+
 $(document).ready(function(){
 	
-	var winWidth = $(window).width();
-	var winHeight = $(window).height();
+	var width = $(window).width();
+	var height = $(window).height();
 
 	// var svg = d3.select('.container').append('svg').attr('width','100%').attr('height',winHeight);
 	// var trigger = svg.append('circle').attr('cx',winWidth/2).attr('cy',winHeight/2).attr('r','50').attr('fill','rgb(240,240,240)');
@@ -38,13 +78,31 @@ $(document).ready(function(){
 
 	/////////////////
 
-	//var width = 960,
-	//height = 500;
+	var action = function(graph) {
+  	force
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .start();
+
+  link = link.data(graph.links)
+    .enter().append("line")
+    .attr("class", "link");
+
+  node = node.data(graph.nodes)
+    .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 12)
+      .on("dblclick", dblclick)
+      .call(drag);
+
+	}
+    /////////////
+	
 
 	var fill = d3.scale.category20();
 
 	var force = d3.layout.force()
-	    .size([winWidth, winHeight])
+	    .size([width, height])
 	    .nodes([{}]) // initialize with a single node
 	    .linkDistance(30)
 	    .charge(-60)
